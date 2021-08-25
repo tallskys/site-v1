1031ed6ee916d65bd42c545af421a07814b523c4
tallskys <tallsky89@gmail.com>
2021-08-25 17:48:15 -0400

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,11 @@ import { graphql } from 'gatsby'
 import get from 'lodash/get'
 
 import Layout from '../components/layout'
-import Hero from '../components/hero'
 import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
   render() {
     const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-    const [author] = get(this, 'props.data.allContentfulPerson.nodes')
 
     return (
       <Layout location={this.props.location}>
